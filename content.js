9fab857147ad5294feb1d1e381db719f3ad0a146
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-28 19:40:05 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,14 @@
+$(function() {
+    $("*")
+        .css({
+            background: "black",
+             color: "orange"
+        }), $("a")
+        .css({
+            color: "orange",
+            border: "orange 1px dotted"
+        }), $("body")
+        .css({
+            color: "orange"
+        })
+});
